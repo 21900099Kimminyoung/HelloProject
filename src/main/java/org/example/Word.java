fa425c9a024cf7c31fc33cfeef35d02178083888
Minyoung Kim <112406111+MinyoungKim@users.noreply.github.com>
2022-09-02 10:16:18 +0900

    create Hello Project

---
+++ src/main/java/org/example/Word.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Word {
+    private string name;
+    private string meaning;
+
+
+    public string getName() {
+        return name;
+    }
+
+    public string getMeaning() {
+        return meaning;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+    public void setMeaning(string meaning) {
+        this.meaning = meaning;
+    }
+}
+//가장 먼저 데이터 구조 만들기 , 안쓰는 변수  = 디폴트
+//단어 관리
