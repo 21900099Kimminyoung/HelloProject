15635a158b2ffc36e0849c0766bbb7d9e7a56ebd
21900099Kimminyoung <21900099@handong.ac.kr>
2022-09-04 22:01:09 +0900

    create Hello Project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        new WordManager().start();
+        new WordManager().start();//WordManager()의 start함수 호출
     }
 }
