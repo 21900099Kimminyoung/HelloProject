f632c861687a2b9a0a4146a84a012ed8416878d3
21900099Kimminyoung <21900099@handong.ac.kr>
2022-09-06 10:37:46 +0900

    create Hello Project

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -26,7 +26,10 @@ public class WordManager {
     public void start(){
         while (true) {
             int menu = seletMenu();
-            if(menu==0) break;
+            if(menu==0) {
+                System.out.println("\n프로그램 종료! 다음에 만나요~");
+                break;
+            }
             if(menu==4){
                 //add
                 wordCRUD.addWord();
