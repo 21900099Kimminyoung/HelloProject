59d0de552080621f84047e5ae0c3c39c3f273c39
21900099Kimminyoung <21900099@handong.ac.kr>
2022-09-06 10:07:13 +0900

    create Hello Project

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -7,7 +7,7 @@ public class WordManager {
     Scanner sc = new Scanner(System.in);
     public int seletMenu(){
         System.out.print("*** 영단어 마스터 ***\n"
-+"********************\n"
++"*****************\n"
         +"1. 모든 단어 보기\n"
         +"2. 수준별 단어 보기\n"
         +"3. 단어 검색\n"
@@ -16,7 +16,7 @@ public class WordManager {
         +"6. 단어 삭제\n"
         +"7. 파일 저장\n"
         +"0. 나가기\n"
-                +"********************\n"
+                +"*****************\n"
         +"=> 원하는 메뉴는? ");
         return sc.nextInt();
     }
