f97e710d7ee43d9a18f0eb1f5bb876d71f787dc4
21900099Kimminyoung <21900099@handong.ac.kr>
2022-09-17 11:06:15 +0900

    create Hello Project

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -52,6 +52,7 @@ public class WordCRUD implements ICRUD{
         }
         System.out.println("--------------------------------");
     }
+
     public ArrayList<Integer> listAll(String Keyword){
         System.out.println("--------------------------------");
         ArrayList<Integer> idlist = new ArrayList<>();
@@ -66,6 +67,19 @@ public class WordCRUD implements ICRUD{
         }
         System.out.println("--------------------------------");
         return idlist;
+    }
+
+    public void listAll(int level){
+        int j=0;
+        System.out.println("--------------------------------");
+        for(int i = 0; i<list.size();i++) {
+            int level1 = list.get(i).getLevel();
+            if (level1!=level) continue;
+            System.out.print(j + 1 + " ");
+            System.out.println(list.get(i).toString());
+            j++;
+        }
+        System.out.println("--------------------------------");
     }
     public void updateltem() {
         String Keyword;
@@ -135,5 +149,17 @@ public class WordCRUD implements ICRUD{
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void searchLevel(){
+        System.out.print("==> 원하는 레벨은? (1~3) ");
+        int level = s.nextInt();
+        listAll(level);
+    }
+
+    public void searchWord(){
+        System.out.print("=> 원하는 단어는? ");
+        String keyword = s.next();
+        listAll(keyword);
     }
 }
